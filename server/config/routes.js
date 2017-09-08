73a2cd1ddbc6e06c25001cf31b6c1f596dcadc3a
Artuom Kukharenko <a.kukahrenko@paralect.com>
2017-09-08 11:34:08 +0300

    [+]add web server

---
+++ server/config/routes.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const config = require('config');
+const indexRouter = require('koa-router')();
+const fs = require('fs');
+
+const staticPath = path.join(__dirname, './../../client/static');
+let staticFileNames;
+let mainJS;
+let mainCSS;
+
+// if (!config.isDev) {
+//   staticFileNames = fs.readdirSync(staticPath);
+//   mainJS = staticFileNames.find(fileName => path.extname(fileName) === '.js');
+//   mainCSS = staticFileNames.find(fileName => path.extname(fileName) === '.css');
+// }
+
+// match all routes but not files (i.e. routes with dots)
+indexRouter.get(/^((?!\.).)*$/, async (ctx) => {
+  const data = {
+    mainCSS: `/static/${mainCSS}`,
+    mainJS: `/static/${mainJS || 'main.js'}`,
+  };
+
+  return ctx.render('index', data);
+});
+
+module.exports = indexRouter.routes();
