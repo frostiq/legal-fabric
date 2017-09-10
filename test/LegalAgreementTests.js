5fd38803a62df877e486f923ddc297cbabc1bbbc
Alex Bazhanau <bazhanau@outlook.com>
2017-09-10 12:10:49 +0300

    LegalAgreement tests added

--- test/LegalAgreementTests.js
+++ test/LegalAgreementTests.js
@@ -1,6 +1,7 @@
 const BigNumber = require('big-number')
 const chai = require("chai")
 const chaiAsPromised = require("chai-as-promised")
+const utils = require("./utils.js")
 
 const LegalAgreement = artifacts.require("./LegalAgreement.sol")
 
@@ -14,6 +15,7 @@ contract('LegalAgreement', function(accounts) {
   const REWARD = 100
   const DEPOSIT = 50
   const ORACLES = [accounts[3], accounts[4], accounts[5]]
+  const UNKNOWN = accounts[6]
 
   const State = { Prepairing : 0, Implementing : 1, Fulfilled : 2, Failed : 3, Canceled : 4 }
 
@@ -21,8 +23,7 @@ contract('LegalAgreement', function(accounts) {
   let deadline
 
   let redeploy = async function() {
-    const now = web3.eth.getBlock(web3.eth.blockNumber).timestamp;
-    deadline = now + 100000
+    deadline = utils.now() + 100000
     legalAgreement = await LegalAgreement.new(deadline, REWARD, DEPOSIT, ORACLES)
   }
 
@@ -37,7 +38,7 @@ contract('LegalAgreement', function(accounts) {
 
   it("should set customer correctly", async function() {
     const state = await legalAgreement.getState()
-    assert(state.equals(State.Prepairing))
+    assert(state.equals(State.Prepairing), "state check")
 
     await legalAgreement.setCustomer({from : CUSTOMER, value : REWARD})
     const res = await legalAgreement.customer()
@@ -46,7 +47,7 @@ contract('LegalAgreement', function(accounts) {
 
   it("should set implementer correctly", async function() {
     const state = await legalAgreement.getState()
-    assert(state.equals(State.Prepairing))
+    assert(state.equals(State.Prepairing), "state check")
 
     await legalAgreement.setImplementer({from : IMPLEMENTER, value : DEPOSIT})
     const res = await legalAgreement.implementer()
@@ -55,7 +56,7 @@ contract('LegalAgreement', function(accounts) {
 
   it("should not allow set implementer second time", async function() {
     const state = await legalAgreement.getState()
-    assert(state.equals(State.Implementing))
+    assert(state.equals(State.Implementing), "state check")
 
     const promise = legalAgreement.setImplementer({from : IMPLEMENTER, value : DEPOSIT})
     return promise.should.be.rejected;
@@ -63,26 +64,87 @@ contract('LegalAgreement', function(accounts) {
 
   it("should not allow set customer second time", async function() {
     const state = await legalAgreement.getState()
-    assert(state.equals(State.Implementing))
+    assert(state.equals(State.Implementing), "state check")
 
     const promise = legalAgreement.setCustomer({from : CUSTOMER, value : REWARD})
     return promise.should.be.rejected;
   })
 
-  it("should allow cancel reward", async function() {
+  it("should allow cancel deposot for implementer", async function() {
     await redeploy()
+    await legalAgreement.setImplementer({from : IMPLEMENTER, value : DEPOSIT})
 
     const state = await legalAgreement.getState()
-    assert(state.equals(State.Prepairing))
+    assert(state.equals(State.Prepairing), "state check")
 
-    const balanceBefore = await web3.eth.getBalance(CUSTOMER)
-    await legalAgreement.cancel({from : CUSTOMER})
-    const balanceAfter = await web3.eth.getBalance(CUSTOMER)
-    console.log(balanceBefore)
-    console.log(balanceAfter)
-    assert(balanceAfter.minus(balanceBefore).equals(REWARD))
+    const balanceBefore = await web3.eth.getBalance(legalAgreement.address)
+    await legalAgreement.cancel({from : IMPLEMENTER})
+    const balanceAfter = await web3.eth.getBalance(legalAgreement.address)
+    assert(balanceBefore.minus(balanceAfter).equals(DEPOSIT))
   })
 
+  it("should not allow cancel deposot for implementer second time", async function() {
+    const state = await legalAgreement.getState()
+    assert(state.equals(State.Prepairing), "state check")
+
+    return legalAgreement.cancel({from : IMPLEMENTER}).should.be.rejected
+  })
+
+  it("should allow cancel reward for customer", async function() {
+    await redeploy()
+    await legalAgreement.setCustomer({from : CUSTOMER, value : REWARD})
+
+    const state = await legalAgreement.getState()
+    assert(state.equals(State.Prepairing), "state check")
+
+    const balanceBefore = await web3.eth.getBalance(legalAgreement.address)
+    await legalAgreement.cancel({from : CUSTOMER})
+    const balanceAfter = await web3.eth.getBalance(legalAgreement.address)
+    assert(balanceBefore.minus(balanceAfter).equals(REWARD))
+  })
+
+  it("should not allow cancel reward for customer second time", async function() {
+    const state = await legalAgreement.getState()
+    assert(state.equals(State.Prepairing), "state check")
+
+    return legalAgreement.cancel({from : CUSTOMER}).should.be.rejected
+  })
+
+  it("should not allow to approve agreement to non-oracles", async function() {
+    await redeploy()
+    await legalAgreement.setCustomer({from : CUSTOMER, value : REWARD})
+    await legalAgreement.setImplementer({from : IMPLEMENTER, value : DEPOSIT})
+
+    const state = await legalAgreement.getState()
+    assert(state.equals(State.Implementing), "state check")
+    
+    return legalAgreement.approve("justification", {from : UNKNOWN}).should.be.rejected
+  })
+
+  it("should allow to approve agreement to oracles", async function() {
+    const state = await legalAgreement.getState()
+    assert(state.equals(State.Implementing), "state check")
+
+    await legalAgreement.approve("justification1", {from : ORACLES[0]})
+    await legalAgreement.approve("justification2", {from : ORACLES[1]})
+
+    const res = await legalAgreement.isApproved()
+
+    assert.isTrue(res, "legalAgreement.isApproved")
+  })
+
+  it("should allow finalize agreement agreement afterdeadline, if approved", async function() {
+    const state = await legalAgreement.getState()
+    assert(state.equals(State.Implementing), "state check")
+
+    await utils.increaseTime(deadline - utils.now() + 10)
+    
+    const balanceBefore = await web3.eth.getBalance(IMPLEMENTER)
+    await legalAgreement.finalize()
+    const balanceAfter = await web3.eth.getBalance(IMPLEMENTER)
+
+    assert(balanceAfter.minus(balanceBefore).equals(REWARD + DEPOSIT))
+  })
   
 
   // reward & deposit
