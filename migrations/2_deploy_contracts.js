2db0282a8c54b3a537ceb24b315eeadea52628ee
Alex Bazhanau <bazhanau@outlook.com>
2017-09-11 23:14:19 +0300

    Contracts deployed on prod

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,22 @@
 const LegalFabric = artifacts.require("./LegalFabric.sol");
 const LegalInstantiator = artifacts.require("./LegalInstantiator.sol");
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts) {
+  let fee, beneficiary
+
+  if(network === 'live'){
+    fee = web3.toWei(0.1, 'ether')
+    beneficiary = '0xdd21D75DB9ed2fe97775ffA46E8FA1C8072Cd15d'
+  }
+  else{
+    fee = 0
+    beneficiary = accounts[0]
+  }
+
   return deployer.deploy(LegalInstantiator)
     .then(() => LegalInstantiator.deployed())
     .then((instantiator) => 
-      deployer.deploy(LegalFabric, 0, '0x0', instantiator.address)
+      deployer.deploy(LegalFabric, fee, beneficiary, instantiator.address)
       .then(() => instantiator.setBuilder(LegalFabric.address))
     )
 };
